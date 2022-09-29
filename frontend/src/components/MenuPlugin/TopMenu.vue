4ca4b31d8965db75e16810cc53f1dfa102ffb230
Daryl <a2ldaryl@gmail.com>
2022-09-29 14:01:48 +0800

    Added top menu components.

---
+++ frontend/src/components/MenuPlugin/TopMenu.vue
@@ -0,0 +1,47 @@
+<template>
+    <div class="sidebar">
+      <div class="sidebar-wrapper">
+        <div class="logo">
+          <a href="#" class="simple-text">
+            {{title}}
+          </a>
+        </div>
+  
+        <slot name="content"></slot>
+        <ul>
+          <!--By default vue-router adds an active class to each route link. This way the links are colored when clicked-->
+          <slot>
+            <sidebar-link v-for="(link,index) in menuBarLinks"
+                          :key="link.name + index"
+                          :to="link.path"
+                          :link="link">
+              <p>{{link.name}}</p>
+            </sidebar-link>
+          </slot>
+        </ul>
+      </div>
+    </div>
+  </template>
+  <script>
+    import TopMenuLink from './TopMenuLink.vue'
+  
+    export default {
+      components: {
+        TopMenuLink
+      },
+      props: {
+        title: {
+          type: String,
+          default: 'Learning Journey Management System'
+        },
+        menuBarLinks: {
+          type: Array,
+          default: () => []
+        },
+      },
+    }
+  
+  </script>
+  <style>
+   
+  </style>
