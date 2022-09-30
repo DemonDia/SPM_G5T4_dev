9e28245c0282eb03c41188927135d1af491934a5
Daryl <a2ldaryl@gmail.com>
2022-10-01 00:10:29 +0800

    SPMPROJ-43 Minor Changes to the RoleView

--- frontend/src/views/RoleView.vue
+++ frontend/src/views/RoleView.vue
@@ -1,46 +1,66 @@
 <template>
   <DashboardLayout>
-    <h1>ROLES</h1>
-
-    <div v-for="(value,key) in roles">
-        <card-component :title=value.title :desc=value.desc />
+    <div class="container-fluid">
+      <div v-for="(value, key) in roles">
+        <card-component :title="value.title" :desc="value.desc"/>
+      </div>
     </div>
   </DashboardLayout>
-
 </template>
 
 <script>
-import DashboardLayout from './Dashboard/Layout/DashboardLayout.vue';
-import CardComponent from '../components/CardComponent.vue';
-import axios from 'axios';
+import DashboardLayout from "./Dashboard/Layout/DashboardLayout.vue";
+import CardComponent from "../components/CardComponent.vue";
+import axios from "axios";
 
 export default {
   name: "RoleView",
   components: {
     DashboardLayout,
-    CardComponent
-},
-  mounted() {
-   document.title = "LJMS - Roles";
+    CardComponent,
   },
   data() {
     return {
-      roles: [
+      roles: /* to get roles from database */
+      [
         {
-          title:"Sous Chef - Egg Machine",
-          desc:"Beat 30 eggs in 1 second"
-        }, 
+          title: "Sous Chef - Egg Machine",
+          desc: "Beat 30 eggs in 1 second",
+        },
         {
-          title:"Role 2",
-          desc:"gegg"
-        }
-      ]
+          title: "Rapper",
+          desc: "Sample Description",
+        },
+        {
+          title: "Rapper",
+          desc: "Sample Description",
+        },
+        {
+          title: "Rapper",
+          desc: "Sample Description",
+        },
+        {
+          title: "Rapper",
+          desc: "Sample Description",
+        },
+        {
+          title: "Rapper",
+          desc: "Sample Description",
+        },
+      ],
+      results: [] /* temporary array */,
+      numRoles: 0 /* to populate based on length of array */,
     }
   },
-  setup() {
-    axios.get('http://localhost:3000/roles').then(response => {
-      console.log(response.data);
+  mounted() {
+    document.title = "LJMS - Roles";
+    axios.get("https://api.kanye.rest/").then((response) => {
+      this.roles[1].desc = response.data.quote;
     });
-  }
+  },
 };
 </script>
+
+<style scoped>
+
+</style>
