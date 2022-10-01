b4c45481d59ef7074054e37f0cb30556c9c63557
jamesTan666 <mdcmaesp@gmail.com>
2022-10-01 11:15:16 +0800

    creation of database and update skillmodel

--- Backend/Models/SkillModel.py
+++ Backend/Models/SkillModel.py
@@ -2,8 +2,8 @@ from typing import Optional
 from datetime import datetime
 from sqlmodel import Field, SQLModel
 
-class Skill(SQLModel,table=True):
-    __tablename__: "skill"
+class SkillModel(SQLModel,table=True):
+    __tablename__: "skillmodel"
     SkillId: Optional[int] = Field(default=None, primary_key=True)
     SkillName: str
     SkillDescription: str
