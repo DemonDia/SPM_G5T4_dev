3eb3a162b0d5971d6860009306344d6205b66948
Setsuna Yukiii <siang-meng@live.com>
2022-10-03 20:43:45 +0800

    Update database.py

--- Backend/database.py
+++ Backend/database.py
@@ -1,5 +1,5 @@
 from sqlmodel import  SQLModel, create_engine, Session
-from config import database_route
+from Backend.config import database_route
 from Models.TrackModel import TrackModel
 
 engine = create_engine(database_route,echo=True)
