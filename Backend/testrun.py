1f60b46400780502000c1b84fe7adca0c7da7b0f
amandasqh <34236882+amandasqh@users.noreply.github.com>
2022-10-04 01:34:09 +0800

    back to siangs

--- Backend/testrun.py
+++ Backend/testrun.py
@@ -3,99 +3,70 @@
 # from HelperFunctions import *
 # from ErrorHandler import *
 
-# from multiprocessing import Process
-# import psutil
-# import pytest
-# import requests
-# import time
-# import uvicorn
-
-# HOST = "127.0.0.1"
-# PORT = 8765
-# WORKERS = 3
-
-# def run_server(host: str, port: int, workers: int, wait: int = 15) -> Process:
-#     proc = Process(
-#         target=uvicorn.run,
-#         args=("main:app",),
-#         kwargs={
-#             "host": host,
-#             "port": port,
-#             "workers": workers,
-#         },
-#     )
-#     proc.start()
-#     time.sleep(wait)
-#     assert proc.is_alive()
-#     return proc
-
-# def shutdown_server(proc: Process):
-
-#     ##### SOLUTION #####
-#     pid = proc.pid
-#     parent = psutil.Process(pid)
-#     for child in parent.children(recursive=True):
-#         child.kill()
-#     ##### SOLUTION END ####
-
-#     proc.terminate()
-#     for _ in range(5):
-#         if proc.is_alive():
-#             time.sleep(5)
-#         else:
-#             return
-#     else:
-#         raise Exception("Process still alive")
-
-# def check_response(host: str, port: int):
-#     assert requests.get(f"http://{host}:{port}").text == '"OK"'
-
-
-# def check_response_time(host: str, port: int, tol: float = 1e-2):
-#     s = time.time()
-#     requests.get(f"http://{host}:{port}")
-#     e = time.time()
-#     assert e-s < tol
-
-# @pytest.fixture(scope="session")
-# def server():
-#     proc = run_server(HOST, PORT, WORKERS)
-#     try:
-#         yield
-#     finally:
-#         shutdown_server(proc)
-
-# def test_main(server):
-#     check_response(HOST, PORT)
-#     check_response_time(HOST, PORT)
-#     check_response(HOST, PORT)
-#     check_response_time(HOST, PORT)
-
+from multiprocessing import Process
+import psutil
+import pytest
+import requests
 import time
 import uvicorn
-import asyncio
-import sys
-from fastapi import FastAPI
-from pydantic import BaseModel
-app = FastAPI()
 
-@app.get("/kill")
-async def kill():
-    global server
-    global loop
-    loop.stop()
-    await server.shutdown()
+HOST = "127.0.0.1"
+PORT = 8765
+WORKERS = 3
 
-config = uvicorn.Config(app=app, host="127.0.0.1", port=5000, log_level="debug")
-server = uvicorn.Server(config=config)
-loop = None
+def run_server(host: str, port: int, workers: int, wait: int = 15) -> Process:
+    proc = Process(
+        target=uvicorn.run,
+        args=("main:app",),
+        kwargs={
+            "host": host,
+            "port": port,
+            "workers": workers,
+        },
+    )
+    proc.start()
+    time.sleep(wait)
+    assert proc.is_alive()
+    return proc
 
-if __name__ == "__main__":
-	try:
-		config.setup_event_loop()
-		loop = asyncio.get_event_loop()
-		loop.run_until_complete(server.serve())
-	except:
-		print(sys.exc_info())
-	while True:
-		time.sleep(1)
+def shutdown_server(proc: Process):
+
+    ##### SOLUTION #####
+    pid = proc.pid
+    parent = psutil.Process(pid)
+    for child in parent.children(recursive=True):
+        child.kill()
+    ##### SOLUTION END ####
+
+    proc.terminate()
+    for _ in range(5):
+        if proc.is_alive():
+            time.sleep(5)
+        else:
+            return
+    else:
+        raise Exception("Process still alive")
+
+def check_response(host: str, port: int):
+    assert requests.get(f"http://{host}:{port}").text == '"OK"'
+
+
+def check_response_time(host: str, port: int, tol: float = 1e-2):
+    s = time.time()
+    requests.get(f"http://{host}:{port}")
+    e = time.time()
+    assert e-s < tol
+
+@pytest.fixture(scope="session")
+def server():
+    proc = run_server(HOST, PORT, WORKERS)
+    try:
+        yield
+    finally:
+        shutdown_server(proc)
+
+def test_main(server):
+    check_response(HOST, PORT)
+    check_response_time(HOST, PORT)
+    check_response(HOST, PORT)
+    check_response_time(HOST, PORT)
