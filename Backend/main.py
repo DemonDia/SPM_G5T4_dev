50632dc1b19c8ed471f072e51b44dfa981c27c8e
Setsuna Yukiii <siang-meng@live.com>
2022-10-03 19:20:47 +0800

    Revert "remove startup"
    This reverts commit 165de100b51857d70548d8a47595c98bf5ae1e2b.

--- Backend/main.py
+++ Backend/main.py
@@ -1,9 +1,14 @@
+
+from Models.TrackModel import TrackModel
 from config import app
 from Routes.TrackRoutes import *
 from HelperFunctions import *
 from ErrorHandler import *
 # instantiate the FasAPI app
 
+@app.on_event("startup")
+async def startup_event():
+    seedInitialData("track",TrackModel)
 
 
 @app.get("/")
