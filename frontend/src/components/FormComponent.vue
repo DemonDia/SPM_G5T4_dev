836cfc599dd05ebf0dda117eab9a2ad982dd797a
Daryl <a2ldaryl@gmail.com>
2022-10-04 16:57:22 +0800

    SPMPROJ-38 initial commit with createrole and formcomponent.

---
+++ frontend/src/components/FormComponent.vue
@@ -0,0 +1,43 @@
+<template>
+<div class="row d-flex justify-content-center m-5">
+    <div class="col-auto">
+        <label class="form-label" v-if="label">{{ label }}</label>
+    </div>
+    <div class="col-sm-4">
+        <input
+        v-bind="$attrs"
+        :value="modelValue"
+        :placeholder="label"
+        @input="$emit('update:modelValue', $event.target.value)"
+        class="form-control"
+        >
+    </div>
+</div>
+  </template>
+  
+  <script>
+  export default {
+    props: {
+      label: {
+        type: String,
+        default: ''
+      },
+      modelValue: {
+        type: [String, Number],
+        default: ''
+      }
+    }
+  }
+  </script>
+  <style scoped>
+  @import url("https://fonts.googleapis.com/css2?family=Poppins:wght@200;300;400;500;600;700&display=swap");
+  
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+    font-family: "Poppins", sans-serif;
+  }
+ 
+  </style>
+  
---
+++ frontend/src/views/CreateRole.vue
@@ -0,0 +1,47 @@
+<template>
+    <DashboardLayout>
+    <div class="container-fluid">
+      <h1>Create a role</h1>
+      <form>
+  
+        <FormComponent
+          v-model="event.title"
+          label="Role Name"
+          type="text"
+        />
+  
+        <FormComponent
+          v-model="event.description"
+          label="Role Description"
+          type="text"
+        />
+  
+        <button class="btn btn-secondary m-3" type="reset">Reset</button>
+        <button class="btn btn-primary" type="submit">Submit</button>
+      </form>
+
+    </div>
+    </DashboardLayout>
+  </template>
+  
+  <script>
+    import DashboardLayout from "./Dashboard/Layout/DashboardLayout.vue";
+    import FormComponent from "../components/FormComponent.vue";
+
+
+  export default {
+    name: "RoleView",
+    components: {
+        DashboardLayout,
+        FormComponent,
+    },
+    data () {
+      return {
+        event: {
+          title: '',
+          description: ''
+        }
+      }
+    }
+  }
+  </script>
