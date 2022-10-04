ff8baf536e433dd1bfd398f19cbc7115c9c4eb93
amandasqh <34236882+amandasqh@users.noreply.github.com>
2022-10-04 18:30:53 +0800

    SPMPROJ-47 skills base template + card fix

--- frontend/src/views/SkillView.vue
+++ frontend/src/views/SkillView.vue
@@ -1,25 +1,74 @@
 <template>
   <DashboardLayout>
-    <div class="home">
-      <img alt="Vue logo" src="../assets/logo.png" />
-      <HelloWorld msg="Welcome to Your Vue.js App" />
-    </div>
+    <div class="container-fluid">
+      <div class="row row-cols-1 row-cols-md-2 row-cols-lg-4">
+        <div v-for="(value, key) in skills" v-bind:key="key">
+          <card-component :title="value.title" :desc="value.desc" :active="value.active" />
+        </div>
+      </div>
+      </div>
   </DashboardLayout>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
-import DashboardLayout from "@/views/Dashboard/Layout/DashboardLayout.vue";
+import DashboardLayout from "./Dashboard/Layout/DashboardLayout.vue";
+import CardComponent from "../components/CardComponent.vue";
+import axios from "axios";
 
 export default {
   name: "SkillView",
   components: {
-    HelloWorld,
     DashboardLayout,
+    CardComponent,
+  },
+  data() {
+    return {
+      skills: /* to get roles from database */
+      [
+        {
+          title: "Beating Eggs",
+          desc: "Masterchef level",
+          active: true,
+        },
+        {
+          title: "Rapping Nicki Minaj",
+          desc: "No cap",
+          active: true,
+        },
+        {
+          title: "Opening soju bottle",
+          desc: "With all the fluff and bartended skills",
+          active: false,
+        },
+        {
+          title: "Eating",
+          desc: "Sample Description",
+          active: true,
+        },
+        {
+          title: "Eating",
+          desc: "Sample Description",
+          active: false,
+        },
+        {
+          title: "Eating",
+          desc: "Sample Description",
+          active: false,
+        },
+      ],
+      results: [] /* temporary array */,
+      numSkills: 0 /* to populate based on length of array */,
+    }
   },
   mounted() {
-   document.title = "LJMS - Skills";
+    document.title = "LJMS - Skills";
+    axios.get("https://api.kanye.rest/").then((response) => {
+      this.roles[1].desc = response.data.quote;
+    });
   },
 };
 </script>
+
+<style scoped>
+
+</style>
