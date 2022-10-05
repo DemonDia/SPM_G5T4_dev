05938dec987dbd1545454abf90ee9c5d53ebaf2b
Daryl <a2ldaryl@gmail.com>
2022-10-05 21:47:38 +0800

    alert

--- frontend/src/views/CreateRole.vue
+++ frontend/src/views/CreateRole.vue
@@ -2,10 +2,32 @@
   <DashboardLayout>
     <div class="container-fluid">
       <h1>Create a role</h1>
+
+      <div class="alert alert-danger" v-if="errors.length">
+        <b>Please correct the following error(s):</b>
+        <ul>
+          <li v-for="(error, index) in errors" v-bind:key="index">
+            {{ error }}
+          </li>
+        </ul>
+      </div>
+
       <form @submit.prevent="createRole" method="POST">
-        <FormComponent v-model="event.role_name" label="Role Name" type="text" limit="30" />
-        <FormComponent v-model="event.role_description" label="Role Description" type="text" limit="170" />
-        <button class="btn btn-secondary m-3" @click="resetForm" type="reset">Reset</button>
+        <FormComponent
+          v-model="event.role_name"
+          label="Role Name"
+          type="text"
+          limit="30"
+        />
+        <FormComponent
+          v-model="event.role_description"
+          label="Role Description"
+          type="text"
+          limit="170"
+        />
+        <button class="btn btn-secondary m-3" @click="resetForm" type="reset">
+          Reset
+        </button>
         <button class="btn btn-primary" type="submit">Submit</button>
       </form>
     </div>
@@ -29,34 +51,35 @@ export default {
         role_name: "",
         role_description: "",
       },
-      errors: [],
+      errors: ["Role already exists! Please try again","Job Description exceeds character limit of 300! Please try again"],
     };
   },
   methods: {
-
-  /// hi  
     createRole() {
       axios
         .post("http://localhost:3000/roles", {
-            role_name: this.event.role_name,
-            role_description: this.event.role_description,
+          role_name: this.event.title,
+          role_description: this.event.description,
         })
         .then((response) => {
-            console.log(response);
+          console.log(response);
         })
         .catch((error) => {
-            this.errors = [];
-            this.errors.push(error.data.m);
-            console.log(error);
+          this.errors = [];
+          this.errors.push(error.data.message);
+          console.log(error);
         });
     },
     resetForm() {
-        this.event.role_name = "";
-        this.event.role_description = "";
+      this.event.title = "";
+      this.event.description = "";
     },
   },
   mounted() {
     document.title = "LJMS - Create Roles";
+    axios.get("https://api.kanye.rest/").then((response) => {
+      console.log(response.data.quote);
+    });
   },
 };
 </script>
