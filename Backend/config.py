96307adc80bfcf343262bfcd4f523d26f20b5a7d
Setsuna Yukiii <siang-meng@live.com>
2022-10-05 17:20:01 +0800

    fixing of cors error

--- Backend/config.py
+++ Backend/config.py
@@ -1,6 +1,8 @@
 from fastapi import FastAPI
 from os import getenv
 from dotenv import load_dotenv
+from fastapi.middleware.cors import CORSMiddleware
+
 # ============this loads the environment variables from .env============
 load_dotenv()
 
@@ -9,3 +11,15 @@ database_route = getenv("DATABASE_URL")
 # instantiate the FasAPI app
 app = FastAPI()
 # adding of middleware
+
+# CORS 
+origins = [
+    "http://localhost:8081",
+]
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
