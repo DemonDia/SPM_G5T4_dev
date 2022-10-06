09a103fab95fc72648e213f0cafbc6628c01e7e9
Setsuna Yukiii <siang-meng@live.com>
2022-10-06 21:23:28 +0800

    fix: validation error messages & cases of empty input

--- Backend/Routes/SkillRoutes.py
+++ Backend/Routes/SkillRoutes.py
@@ -20,35 +20,53 @@ def addSeedData():
 
 @app.get('/skills/')
 def getSkills(session: Session = Depends(get_session)):
-    stmt = select(SkillModel)
-    result = session.exec(stmt).all()
-    # return result
-    return {
-        "success": True,
-        "data": result
-    }
+    errors = []
+    try:
+        stmt = select(SkillModel)
+        result = session.exec(stmt).all()
+        # return result
+        return {
+            "success": True,
+            "data": result
+        }
+    except Exception as e:
+        errors.append(str(e))
+        return {
+            "success": False,
+            "message": errors
+        }
 
 
 @app.post('/skills/')
 def CreateSkills(skill: SkillModel, session: Session = Depends(get_session)):
+    errors = []
     try:
-        findDuplicateRoleStatement = select(SkillModel).where(SkillModel.skill_name == skill.skill_name)
+        findDuplicateRoleStatement = select(SkillModel).where(
+            SkillModel.skill_name == skill.skill_name)
         results = session.exec(findDuplicateRoleStatement)
         for duplicateRoles in results:
-            return {
-                "success": False,
-                "message": "Skill already exists! Please try again"
-            }
+            errors.append("Skill already exists! Please try again")
+            break
+
+        if len(skill.skill_name) == 0:
+            errors.append("Skill name cannot be empty! Please try again")
+
         if len(skill.skill_name) > 30:
-            return {
-                "success": False,
-                "message": "Skill name exceeds character limit of 30! Please try again"
-            }
-        # check for description length limit
+            errors.append(
+                "Skill name exceeds character limit of 30! Please try again")
+
+        if len(skill.skill_description) == 0:
+            errors.append(
+                "Skill Description cannot be empty! Please try again")
+
         if len(skill.skill_description) > 170:
+            errors.append(
+                "Skill Description exceeds character limit of 170! Please try again")
+
+        if (len(errors) > 0):
             return {
                 "success": False,
-                "message": "Skill Description exceeds character limit of 170! Please try again"
+                "message": errors
             }
         session.add(skill)
         session.commit()
@@ -56,10 +74,11 @@ def CreateSkills(skill: SkillModel, session: Session = Depends(get_session)):
         session.close()
         return {
             "success": True,
-            "message": "Successfully added"
+            "message": "Skill have been added"
         }
     except Exception as e:
-        return{
+        errors.append(str(e))
+        return {
             "success": False,
-            "message": str(e)
+            "message": errors
         }
