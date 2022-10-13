4a87b004b4c267a983644c0bd56c0b2b94392444
Setsuna Yukiii <siang-meng@live.com>
2022-10-13 19:47:28 +0800

    adding routes for assigning skill to role

--- Backend/Routes/RoleSkillRelationRoutes.py
+++ Backend/Routes/RoleSkillRelationRoutes.py
@@ -3,23 +3,47 @@ from Schema.RoleSkillRelationSchema import RoleSkillRelation
 from database import *
 from sqlmodel import Session, select, delete
 from config import app
-from Models.RoleModel import RoleModel
+from Models.IndependentModels import *
 from HelperFunctions import *
 # ===========================test functions===========================
 
 
 # ===========================actual CRUD functions===========================
-@app.post('/roleskilllink/')
-def getRoles(session: Session = Depends(get_session)):
+@app.post('/roleskillrelations/')
+def addRoleSkillRelation(roleSkillRelation: RoleSkillRelation, session: Session = Depends(get_session)):
     errors = []
-    return
     try:
-        stmt = select(RoleModel)
-        result = session.exec(stmt).all()
-        # return result
+        # put None as default; declare within the function
+        # role = None
+        # skill = None
+        # if(roleSkillRelation.Role_ID == None):
+        #     errors.append("Role is not selected")
+
+        # else:
+        #     role = session.get(RoleModel, roleSkillRelation.Role_ID)
+
+        # if(roleSkillRelation.Skill_ID == None):
+        #     errors.append("Skill is not selected")
+        # else:
+
+        role = session.get(RoleModel, roleSkillRelation.Role_ID)
+        skill = session.get(SkillModel, roleSkillRelation.Skill_ID)
+        if role == None:
+            errors.append("Role does not exist!")
+        if skill == None:
+            errors.append("Skill does not exist")
+        if len(errors > 0):
+            return {
+                "success": False,
+                "message": errors
+            }
+        session.add(roleSkillRelation)
+        session.commit()
+        session.refresh(roleSkillRelation)
+        session.close()
         return {
             "success": True,
-            "data": result
+            "message": "Skill assigned to role"
         }
     except Exception as e:
         errors.append(str(e))
@@ -27,5 +51,3 @@ def getRoles(session: Session = Depends(get_session)):
             "success": False,
             "message": errors
         }
-
-
