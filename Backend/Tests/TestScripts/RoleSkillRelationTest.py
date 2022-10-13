24d3413f841025f60989afb44a9d8594f4316b99
Setsuna Yukiii <siang-meng@live.com>
2022-10-13 19:31:24 +0800

    test script for role skill relation

---
+++ Backend/Tests/TestScripts/RoleSkillRelationTest.py
@@ -0,0 +1,24 @@
+from HelperFunctionForTest import *
+
+# ======================Test cases======================
+entityName = "roleskillrelations"
+
+
+def addRoleTest():
+    print("User Story: Assign Skill To Role")
+    triggerTestCase("Both role and skill exist", True, entityName,
+                    {
+                        "Role_ID": 1,
+                        "Skill_ID": 1,
+                    }, "addRelation"
+                    )
+    triggerTestCase("Role does not exist", False, entityName,
+                    {
+                        "Role_ID": -1,
+                        "Skill_ID": 1,
+                    }, "addRelation")
+    triggerTestCase("Skill does not exist", False, entityName,
+                    {
+                        "Role_ID": 1,
+                        "Skill_ID": -1,
+                    }, "addRelation")
