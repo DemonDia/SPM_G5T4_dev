65b79adb643ae7789cd988d8a2417dbb3f0ecb65
jamesTan666 <mdcmaesp@gmail.com>
2022-10-14 16:12:54 +0800

    adding of CourseSkillRelation (Create)

--- Backend/Models/IndependentModels.py
+++ Backend/Models/IndependentModels.py
@@ -2,6 +2,17 @@ from typing import List, Optional
 from datetime import datetime
 from sqlmodel import Field, SQLModel,Relationship
 from Models.RoleSkillRelationModel import RoleSkillRelationModel
+from Models.CourseSkillRelationModel import CourseSkillRelationModel
+
+class CourseModel(SQLModel,table=True):
+    __tablename__: "coursemodel"
+    Course_ID: Optional[str] = Field(default=None, primary_key=True)
+    Course_Name: str
+    Course_Desc: str
+    Course_Status: str
+    Course_Type: str
+    Course_Category: str
+    Skills: List['SkillModel'] = Relationship(back_populates="Courses", link_model=CourseSkillRelationModel)
 
 
 class SkillModel(SQLModel,table=True):
@@ -11,6 +22,7 @@ class SkillModel(SQLModel,table=True):
     Skill_Description: str
     Active: bool
     Roles: List['RoleModel'] = Relationship(back_populates="Skills", link_model=RoleSkillRelationModel)
+    Courses: List['CourseModel'] = Relationship(back_populates="Skills", link_model=CourseSkillRelationModel)
 
 class RoleModel(SQLModel,table=True):
     __tablename__: "rolemodel"
@@ -19,6 +31,3 @@ class RoleModel(SQLModel,table=True):
     Role_Description:str
     Active:bool
     Skills: List['SkillModel'] = Relationship(back_populates="Roles", link_model=RoleSkillRelationModel)
-
-
-
---
+++ Backend/Routes/CourseSkillRelationRoutes.py
@@ -0,0 +1,48 @@
+from fastapi import Response, Depends
+from Models.CourseSkillRelationModel import CourseSkillRelationModel
+from database import *
+from sqlmodel import Session, select, delete
+from config import app
+from Models.IndependentModels import *
+from HelperFunctions import *
+# ===========================test functions===========================
+
+
+@app.delete("/courseSkillRelations/deleteall")
+def deleteAll():
+    return deleteAllData(CourseSkillRelationModel)
+
+
+# ===========================actual CRUD functions===========================
+@app.post('/courseSkillRelations/')
+def addcourseSkillRelation(courseSkillRelation: CourseSkillRelationModel, session: Session = Depends(get_session)):
+    errors = []
+    try:
+        print("courseSkillRelation", courseSkillRelation)
+        print("courseSkillRelation", courseSkillRelation.Role_ID)
+        print("courseSkillRelation", courseSkillRelation.Skill_ID)
+        course = session.get(CourseModel, courseSkillRelation.Role_ID)
+        skill = session.get(SkillModel, courseSkillRelation.Skill_ID)
+        if course == None:
+            errors.append("Course does not exist!")
+        if skill == None:
+            errors.append("Skill does not exist")
+        if len(errors) > 0:
+            return {
+                "success": False,
+                "message": errors
+            }
+        session.add(courseSkillRelation)
+        session.commit()
+        session.refresh(courseSkillRelation)
+        session.close()
+        return {
+            "success": True,
+            "message": "Skill assigned to course"
+        }
+    except Exception as e:
+        errors.append(str(e))
+        return {
+            "success": False,
+            "message": errors
+        }
