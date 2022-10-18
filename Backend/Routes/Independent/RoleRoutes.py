46b7601693cbf22186139ca1dd18630071b0e3c4
Setsuna Yukiii <siang-meng@live.com>
2022-10-18 12:51:54 +0800

    hard deleting relations

--- Backend/Routes/Independent/RoleRoutes.py
+++ Backend/Routes/Independent/RoleRoutes.py
@@ -20,10 +20,13 @@ def addSeedData():
     return seedInitialData("role", RoleModel)
 
 # ===========================helper functions===========================
+
+
 def getRelatedSkills(targetModelIdValue):
     try:
         session = Session(engine)
-        statement = select(SkillModel.Skill_Name).select_from(join(SkillModel,RoleSkillRelationModel)).where(RoleSkillRelationModel.Skill_ID == SkillModel.Skill_ID)
+        statement = select(SkillModel.Skill_Name).select_from(join(SkillModel, RoleSkillRelationModel)).where(
+            RoleSkillRelationModel.Skill_ID == SkillModel.Skill_ID)
         results = session.exec(statement).all()
         return {
             "success": True,
@@ -33,31 +36,36 @@ def getRelatedSkills(targetModelIdValue):
         return {
             "success": False,
             "messaage": e,
-            "data":[]
+            "data": []
         }
 # returns json
+
+
 def getAllRelatedSkills():
     try:
         skillDict = {}
         session = Session(engine)
-        statement = select(SkillModel.Skill_Name,RoleModel.Role_ID).select_from(join(RoleModel,join(SkillModel,RoleSkillRelationModel)))
+        statement = select(SkillModel.Skill_Name, RoleModel.Role_ID).select_from(
+            join(RoleModel, join(SkillModel, RoleSkillRelationModel)))
         results = session.exec(statement).all()
         for result in results:
-            if(result.Role_ID not in skillDict.keys()):
+            if (result.Role_ID not in skillDict.keys()):
                 skillDict[result.Role_ID] = [result.Skill_Name]
             else:
                 skillDict[result.Role_ID].append(result.Skill_Name)
-        return{
-            "success":True,
-            "data":skillDict
+        return {
+            "success": True,
+            "data": skillDict
         }
     except Exception as e:
-        return{
-            "success":False,
-            "message":e,
-            "data":[]
+        return {
+            "success": False,
+            "message": e,
+            "data": []
         }
 # ===========================actual CRUD functions===========================
+
+
 @app.get('/roles/')
 def getRoles(session: Session = Depends(get_session)):
     errors = []
@@ -204,94 +212,109 @@ def createRoles(role: RoleModel, session: Session = Depends(get_session)):
             "message": str(e)
         }
 
-#update role 
-@app.put("/roles/{Role_ID}/") 
-def updateRole(Role_ID: int, updated_role: RoleModel, session: Session = Depends(get_session)): 
-    errors = [] 
-    try: 
-        #skill = session.get(SkillModel, Skill_ID) 
-        statement = select(RoleModel).where(RoleModel.Role_ID == Role_ID) 
-        result = session.exec(statement) 
-        role = result.one() 
- 
-        if role == None: 
-            errors.append("Role not found!") 
- 
- 
-            findDuplicateRoleStatement = select(RoleModel).where( 
-                RoleModel.Role_Name == updated_role.Role_Name) 
-            results = session.exec(findDuplicateRoleStatement).one() 
- 
-            # check for duplicate skill name 
-            if results: 
-                errors.append("Role already exists! Please try again") 
- 
- 
-            # check for empty skill name 
-            if len(updated_role.Role_Name) == 0: 
-                errors.append("Role Name cannot be empty! Please try again") 
- 
-            # check if skill name exceeds 30 characters 
-            if len(updated_role.Role_Name) > 30: 
-                errors.append( 
-                    "Role Name exceeds character limit of 30! Please try again") 
- 
-            # check for empty skill description 
-            if len(updated_role.Role_Description) == 0: 
-                errors.append( 
-                    "Role Description cannot be empty! Please try again") 
- 
-            # check if skill name exceeds 170 characters 
-            if len(updated_role.Role_Description) > 170: 
-                errors.append( 
-                    "Role Description exceeds character limit of 170! Please try again") 
- 
-        if (len(errors) > 0): 
-            return { 
-                "success": False, 
-                "message": errors 
-            } 
-        if updated_role.Role_Name: 
-            role.Role_Name = updated_role.Role_Name 
-        if updated_role.Role_Description: 
-            role.Role_Description = updated_role.Role_Description 
- 
-        session.add(role) 
-        session.commit() 
-        session.refresh(role) 
-        session.close() 
-        return { 
-            "success": True, 
-            "message": "Successfully updated" 
-        } 
-    except Exception as e: 
-        errors.append(str(e)) 
-        return{ 
-        "success":False, 
-        "message":errors 
-        } 
+# update role
 
-# softDeleteRole 
-@app.put("/roles/delete/{Role_ID}/") 
-def softDeleteRole(Role_ID: int,session: Session = Depends(get_session)): 
-    #skill = session.get(SkillModel).where(SkillModel.Skill_Name == Skill_Name) 
-    statement = select(RoleModel).where(RoleModel.Role_ID == Role_ID) 
-    result = session.exec(statement) 
-    role = result.one() 
-    if role == None: 
-        return { 
-            "success": False, 
-            "message": "Role not found " 
-        } 
-    if role.Active: 
-        role.Active = False 
- 
- 
-    session.add(role) 
-    session.commit() 
-    session.refresh(role) 
-    session.close() 
-    return { 
-        "success": True, 
-        "message": "Role deleted" 
-    }
+
+@app.put("/roles/{Role_ID}/")
+def updateRole(Role_ID: int, updated_role: RoleModel, session: Session = Depends(get_session)):
+    errors = []
+    try:
+        #skill = session.get(SkillModel, Skill_ID)
+        statement = select(RoleModel).where(RoleModel.Role_ID == Role_ID)
+        result = session.exec(statement)
+        role = result.one()
+
+        if role == None:
+            errors.append("Role not found!")
+
+            findDuplicateRoleStatement = select(RoleModel).where(
+                RoleModel.Role_Name == updated_role.Role_Name)
+            results = session.exec(findDuplicateRoleStatement).one()
+
+            # check for duplicate skill name
+            if results:
+                errors.append("Role already exists! Please try again")
+
+            # check for empty skill name
+            if len(updated_role.Role_Name) == 0:
+                errors.append("Role Name cannot be empty! Please try again")
+
+            # check if skill name exceeds 30 characters
+            if len(updated_role.Role_Name) > 30:
+                errors.append(
+                    "Role Name exceeds character limit of 30! Please try again")
+
+            # check for empty skill description
+            if len(updated_role.Role_Description) == 0:
+                errors.append(
+                    "Role Description cannot be empty! Please try again")
+
+            # check if skill name exceeds 170 characters
+            if len(updated_role.Role_Description) > 170:
+                errors.append(
+                    "Role Description exceeds character limit of 170! Please try again")
+
+        if (len(errors) > 0):
+            return {
+                "success": False,
+                "message": errors
+            }
+        if updated_role.Role_Name:
+            role.Role_Name = updated_role.Role_Name
+        if updated_role.Role_Description:
+            role.Role_Description = updated_role.Role_Description
+
+        session.add(role)
+        session.commit()
+        session.refresh(role)
+        session.close()
+        return {
+            "success": True,
+            "message": "Successfully updated"
+        }
+    except Exception as e:
+        errors.append(str(e))
+        return {
+            "success": False,
+            "message": errors
+        }
+
+# softDeleteRole
+
+
+@app.put("/roles/delete/{Role_ID}/")
+def softDeleteRole(Role_ID: int, session: Session = Depends(get_session)):
+    try:
+        #skill = session.get(SkillModel).where(SkillModel.Skill_Name == Skill_Name)
+        statement = select(RoleModel).where(RoleModel.Role_ID == Role_ID)
+        result = session.exec(statement)
+        role = result.one()
+        if role == None:
+            return {
+                "success": False,
+                "message": "Role not found "
+            }
+        if role.Active:
+            role.Active = False
+
+        # hard delete the relations
+        allRelatedRelationStatement = select(RoleSkillRelationModel).where(
+            RoleSkillRelationModel.Role_ID == Role_ID)
+        relationResults = session.exec(allRelatedRelationStatement)
+        allRelations = relationResults.all()
+        for relation in allRelations:
+            session.delete(relation)
+
+        session.add(role)
+        session.commit()
+        session.refresh(role)
+        session.close()
+        return {
+            "success": True,
+            "message": "Role deleted"
+        }
+    except Exception as e:
+        return {
+            "success": False,
+            "message": str(e)
+        }
