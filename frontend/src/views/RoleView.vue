877e3744ed76727e4d87133dc0b1d87160521a01
amandasqh <34236882+amandasqh@users.noreply.github.com>
2022-10-24 06:20:12 +0800

    SPMPROJ-60 search bar for role-skill

--- frontend/src/views/RoleView.vue
+++ frontend/src/views/RoleView.vue
@@ -6,14 +6,32 @@
 
       <!-- Dashboard -->
       <div v-else class="">
+
+        <!-- Search by Skills -->
+        <div class="container-sm my-5 mb-3 text-start">
+          <PillSearchComponent ctype="skill" @pillItems="getPill"></PillSearchComponent>
+        </div>
+
+        <!-- Create Role -->
         <div class="row mt-3 mx-auto">
           <div class="d-grid gap-2 d-md-flex justify-content-md-end">
             <router-link to="/create-role" tag="button" class="btn btn-dark btn-lg" v-if=" user.Role == 1">Create Role</router-link>
           </div>
         </div>
+
+        <!-- Role Card -->
         <div class="row row-cols-1 row-cols-md-2 row-cols-lg-4">
           <div v-for="(value, key) in roles" v-bind:key="key">
-            <card-component :title="value.Role_Name" :desc="value.Role_Description" :active="value.Active" :pillList="value.Skills" :id="value.Role_ID" ctype="role" @reload="reload()"/>
+            <card-component 
+              :title="value.Role_Name" 
+              :desc="value.Role_Description" 
+              :active="value.Active" 
+              :pillList="value.Skills" 
+              :id="value.Role_ID" 
+              ctype="role" 
+              @reload="reload()"
+              v-if="this.pillItemsFromComponent.length == 0 || this.isFound(value.Skills)"
+            />
           </div>
         </div>
       </div>
@@ -32,35 +50,68 @@
   import axios from "axios";
   import { mapGetters } from "vuex";
   import SpinnerComponent from "@/components/SpinnerComponent.vue";
+  import PillSearchComponent from '@/components/PillSearchComponent.vue'
 
   export default {
     name: "RoleView",
     components: {
     DashboardLayout,
     CardComponent,
-    SpinnerComponent
+    SpinnerComponent,
+    PillSearchComponent
 },
     data() {
       return {
         roles: [], // roles from database
-        results: [], // temporary array
-        numRoles: 0, // to populate based on length of array
+        numRolesFound: 0,
         noRoleFound: false,
+        availSkills: ['Select skills:'],
+        noSkillFound: false,
+        pillItemsFromComponent: [],
+        selectedSkills: [],
       }
     },
     methods: {
-     async reload() {
-      var url = "https://01p0cxotkg.execute-api.us-east-1.amazonaws.com/dev/roles/available";
-      await axios.get(url).then((response) => {
-        var result = response.data.data
-        this.roles = result
-        if (this.roles.length == 0) {
-          this.noRoleFound = true
+      async reload() {
+        this.pillItemsFromComponent = [];
+        var url = "https://01p0cxotkg.execute-api.us-east-1.amazonaws.com/dev/roles/available";
+        await axios.get(url).then((response) => {
+          var result = response.data.data
+          this.roles = result
+          if (this.roles.length == 0) {
+            this.noRoleFound = true
+          }
+        }).catch((error) => {
+          console.log(error)
+        });
+      },
+
+      getPill(item) {
+        // emit content to be passed into the selectedList
+        this.pillItemsFromComponent = item;
+        this.selectedSkills = item.map(role => {
+          return role.Skill_Name
+        });
+        this.numRolesFound = 0
+      },
+
+      isFound(arr1) {
+        // show roles that include SOME of the skills selected
+        // return (this.selectedSkills.some( x => arr1.includes(x) ));
+
+        // show roles that include ALL of the skills selected
+        if (this.selectedSkills.every( x => arr1.includes(x) )) {
+          this.numRolesFound += 1;
+          if (this.noRoleFound) {
+            this.noRoleFound = false;
+          }
+          return true;
         }
-      }).catch((error) => {
-        console.log(error)
-      });
-    }
+        if (this.numRolesFound == 0) {
+          this.noRoleFound = true;
+        }
+        return false;
+      },
     },
     mounted() {
       document.title = "LJMS - Roles";
