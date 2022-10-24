4cb02ee865010979622badb0f466826e3c8f9ab6
Setsuna Yukiii <siang-meng@live.com>
2022-10-24 20:56:19 +0800

    SPMPROJ-20 creation of test cases

--- Backend/Tests/TestScripts/Dependent/RoleSkillRelationTest.py
+++ Backend/Tests/TestScripts/Dependent/RoleSkillRelationTest.py
@@ -23,17 +23,42 @@ def addSkillsToRoleTest():
                         "Roles": [1, 2, 3]
                     }, "addRelation")
 
+def updateSkillsOfRole():
+    print("Update skills of role")
+    triggerTestCase("Update skills of role,", True, skillToRole,
+                    {
+                        "Skills": [10,11,12]
+                    }, "updateById",2
+                    )
+
+def updateRolesToSkill():
+    print("Update roles of skill")
+    triggerTestCase("Update roles of skill", True, roleToSkill,
+                    {
+                        "Roles": [10, 11, 12]
+                    }, "updateById",2
+                    )
+
+
 def viewSkillsRelatedToRole():
     print("View skills related to role")
     triggerTestCase("View skills related to role", True, skillToRole,
-                    None, "readById",1)
+                    None, "readById", 1)
+
 
 def viewRolesRelatedToSkill():
     print("View roles related to skill")
-    triggerTestCase("View roles related to skill", True, skillToRole,
-                    None, "readById",1)
+    triggerTestCase("View roles related to skill", True, roleToSkill,
+                    None, "readById", 1)
+
+
+
 
 def testAllRoleSkillRelationCases():
     addRoleSkillRelationTest()
     addSkillsToRoleTest()
     viewSkillsRelatedToRole()
+    viewRolesRelatedToSkill()
+    updateSkillsOfRole()
+    updateRolesToSkill()
+    
