7ef2f73486af6094271d40a0af4d42cdb4f1055c
jamesTan666 <mdcmaesp@gmail.com>
2022-10-25 06:58:37 +0800

    Update Unit test case before and after

--- Backend/Tests/TestScripts/Independent/SkillTest.py
+++ Backend/Tests/TestScripts/Independent/SkillTest.py
@@ -28,36 +28,39 @@ def addSkillTest():
 
 def viewExistingSkillTest():
     print("User Story: View Existing Skills")
-    #before = getSingleRow(BASE+entityName+"/available/", 1)
-    #print("before",before)
+    before = getAllRows(BASE+entityName+"/available/")  #get all skills where active=1
+    print("before",before)
     triggerTestCase("View Existing Skills",
                     True, entityName+"/available/", {
                     }, "readAll", None)
-    #after = getSingleRow(BASE+entityName, 1)
-    #print("after",after)
+    after = getAllRows(BASE+entityName+"/available/")
+    print("after",after)
     return
 
 def updatingSkillTest():
     print("User Story: Update Skill")
-    #before = getSingleRow(BASE+entityName, 1)
-    #print("before",before)
+    before = getAllRows(BASE+entityName+"/") #get all skills
+    print("before",before)
     triggerTestCase("Update skill fields",
                     True, entityName, {
                         "Skill_Name": "Fluffing",
                         "Skill_Description": "Thinking on the spot",
                         "Active": True
                     }, "updateById", 1)
-    #after = getSingleRow(BASE+entityName, 1)
-    #print("after",after)
+    after = getAllRows(BASE+entityName+"/")
+    print("after",after)
     return
 
 #do it
 def deleteSkillTest():
     print("User Story: Soft Delete Skill")
-
+    before = getAllRows(BASE+entityName+"/available/")#get all skills where active=1
+    print("before",before)
     triggerTestCase("soft Delete skill fields",
                     True, entityName, {
                     }, "softDelete", 1)
+    after = getAllRows(BASE+entityName+"/available/")
+    print("after",after)
 
     return
 
