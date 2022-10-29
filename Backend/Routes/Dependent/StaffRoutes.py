37ac246d9b182435e44183ad97c8baf8b353199a
Setsuna Yukiii <siang-meng@live.com>
2022-10-29 09:12:24 +0800

    SPMPROJ-25 creating of staff routes

---
+++ Backend/Routes/Dependent/StaffRoutes.py
@@ -0,0 +1,18 @@
+from fastapi import Response, Depends
+from Schema.IndependentSchema import Skill
+from database import *
+from sqlmodel import Session, select, delete, join
+from config import app
+from Models.DependentModels import StaffModel
+from HelperFunctions import *
+
+# ===========================test functions===========================
+
+@app.delete("/staff/deleteall")
+def deleteAll():
+    return deleteAllData(UserRoleModel)
+
+
+@app.post("/staff/seedall")
+def addSeedData():
+    return seedInitialData("staff", StaffModel, 25, False, "staff")
