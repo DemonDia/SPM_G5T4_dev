55b07d63672eb33e6ac461b13d32466a648840bb
Setsuna Yukiii <siang-meng@live.com>
2022-11-02 17:42:24 +0800

    adding of seedall for courselearningjourney

--- Backend/Tests/HelperFunctionForTest.py
+++ Backend/Tests/HelperFunctionForTest.py
@@ -1,7 +1,7 @@
 import requests
 # =====================Universal variables=====================
 # name of entities
-entities = ["roles","skills","course","userroles","roleskillrelations","courseskillrelations","staff","learningjourney","courselearningjourney"]
+entities = ["roles","skills","course","userroles","roleskillrelations","courseskillrelations","staff","learningjourney","courselearningjourney","courselearningjourney"]
 
 # name of operations (based on CRUD)
 # Names:
