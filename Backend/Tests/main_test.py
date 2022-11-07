d91afe075a6bd0cbe6425a60efb1940e516ea60b
Setsuna Yukiii <siang-meng@live.com>
2022-11-07 23:51:43 +0800

    adapting testrun into main_test

--- Backend/Tests/main_test.py
+++ Backend/Tests/main_test.py
@@ -9,12 +9,12 @@ from TestScripts.Dependent.CourseLearningJourneyTest import testAllCourseLearnin
 from TestScripts.Dependent.LearningJourneySkillTest import testAllLearningJourneySkillRelationCases
 import pytest
 
-@pytest.fixture(autouse=True)
-def start():
-    cleanUp()
-    seedAllData()
-    yield
-    # cleanUp()
+# @pytest.fixture(autouse=True)
+# def start():
+#     cleanUp()
+#     seedAllData()
+#     yield
+#     # cleanUp()
 
 
 def mainTest():
@@ -28,8 +28,64 @@ def mainTest():
     testAllLearningJourneySkillRelationCases()
     # remove all the test data after testing is complete
 
-if __name__ == "__main__":
-    cleanUp()
-    seedAllData()
-    mainTest()
-    cleanUp()
+# if __name__ == "__main__":
+#     cleanUp()
+#     seedAllData()
+#     mainTest()
+#     cleanUp()
+
+
+import logging
+from fastapi import FastAPI
+
+class App:
+    """ Core application to test. """
+
+    def __init__(self):
+        self.api = FastAPI()
+        # register endpoints
+        self.api.get("/")(self.read_root)
+        self.api.on_event("shutdown")(self.close)
+
+    async def close(self):
+        """ Gracefull shutdown. """
+        logging.warning("Shutting down the app.")
+
+    async def read_root(self):
+        """ Read the root. """
+        return {"Hello": "World"}
+
+""" Testing part."""
+from multiprocessing import Process
+import asynctest
+import asyncio
+import aiohttp
+import uvicorn
+
+class TestScriptApp(asynctest.TestCase):
+    """ Test the app class. """
+
+    async def setUp(self):
+        """ Bring server up. """
+        app = App()
+        self.proc = Process(target=uvicorn.run,
+                            args=(app.api,),
+                            kwargs={
+                                "host": "127.0.0.1",
+                                "port": 8000,
+                                "log_level": "info"},
+                            daemon=True)
+        self.proc.start()
+        await asyncio.sleep(0.1)  # time for the server to start
+
+    async def tearDown(self):
+        """ Shutdown the app. """
+        self.proc.terminate()
+
+    async def test_read_root(self):
+        """ Fetch an endpoint from the app. """
+        async with aiohttp.ClientSession() as session:
+            cleanUp()
+            seedAllData()
+            mainTest()
+            cleanUp()
