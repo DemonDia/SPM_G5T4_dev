a3ccd6370e13b73becbd7df781b735bbffc05783
amandasqh <34236882+amandasqh@users.noreply.github.com>
2022-11-07 23:13:51 +0800

    ui fixes

--- frontend/src/views/CreateLJ.vue
+++ frontend/src/views/CreateLJ.vue
@@ -92,7 +92,7 @@
                 <div 
                   v-for="(value, key) in this.roles.roles" 
                   :key="key"
-                  class="p-1 overflow-scroll"
+                  class="p-1 overflow-scroll d-sm-block d-md-inline-block"
                 >
                   <TileComponent 
                     :id="value.Role_ID"
@@ -134,7 +134,7 @@
             </div>
             <!-- Course Selection -->
             <div v-else v-if="!noSkillFound">
-              <div class="text-sm-start text-md-center fw-bold my-2">
+              <div class="text-sm-start text-md-center fw-bold my-2 px-3">
                 You have selected: 
                 <span 
                   id="selected" 
@@ -144,7 +144,7 @@
               </div>
               <div 
                 id="tiles"
-                class="d-sm-inline-flex d-md-flex flex-wrap justify-content-around"
+                class="d-sm-inline-flex d-md-flex flex-wrap justify-content-sm-start justify-content-md-center"
               >
                 <div 
                   v-for="(value, key) in this.skills.skills" 
@@ -402,7 +402,6 @@
 
       loadCourses() {
         var url = "https://01p0cxotkg.execute-api.us-east-1.amazonaws.com/dev/skillcourserelations/byid";
-
         axios.post(url, {"Skills": this.selectedS}).then((response) => {
           var result = response.data.data;
           if (result.length == 0) {
