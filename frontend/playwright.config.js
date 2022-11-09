c98a039ace527275bd58622a2259eab896967e99
Daryl <qingyu.tan.2020@sis.smu.edu.sg>
2022-11-09 09:35:58 +0800

    Minor change

--- frontend/playwright.config.js
+++ frontend/playwright.config.js
@@ -37,6 +37,7 @@ const config = {
   /* Run your local dev server before starting the tests */
   webServer: {
     command: 'npm run serve',
+    timeout: 120000,
     port: 8080,
     reuseExistingServer: !process.env.CI,
   },
